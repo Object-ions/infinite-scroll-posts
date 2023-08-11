37ece3508d9f567ca180f83e409af4e164609c1a
object-ions <moshikoatia@gmail.com>
2023-08-10 17:36:04 -0700

    handle res from API. append postEl to the DOM

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
-const postContainer = document.getElementById('post-container');
+const postContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
 let limit = 3;
 let page = 1;
 
+//Fetch posts from API
 async function getPosts() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
 
@@ -12,3 +13,24 @@ async function getPosts() {
 
   return data;
 }
+
+//Show posts in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postContainer.appendChild(postEl);
+  });
+}
+
+showPosts();
