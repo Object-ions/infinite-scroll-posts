ee89cae1bf27fda14e071d646ce984661af121b6
object-ions <moshikoatia@gmail.com>
2023-08-11 18:44:45 -0700

    add filter function to show the search

--- scripts.js
+++ scripts.js
@@ -48,12 +48,36 @@ function showLoader() {
   }, 1000);
 }
 
-showPosts();
-
-window.addEventListener('scroll', () => {
+//Scroll func
+function scroll() {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoader();
   }
-})
+}
+
+//Filter posts by inpu
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  })
+}
+
+//Show initial posts
+showPosts();
+
+//Event listeners
+window.addEventListener('scroll', scroll);
+
+filter.addEventListener('input', filterPosts);
